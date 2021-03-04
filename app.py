965ed0d0fba1b5b969767593d7f8018a10fde3f5
P K <prakhyatkarri@gmail.com>
2021-03-03 21:15:24 -0500

    Added files

--- app.py
+++ app.py
@@ -2,13 +2,18 @@ import json
 
 from api.request import token
 
-if __name__ == '__main__':
-    secrets_path = './config/secrets.json'
+
+def get_secrets():
+    """ Get Config Secrets File data"""
     try:
-        with open(secrets_path) as secret_file:
-            config_secrets = json.load(secret_file)
+        with open('config/secrets.json') as secret_file:
+            return json.load(secret_file)
     except Exception as e:
         print(f'Exception in main: {e}')
+
+
+if __name__ == '__main__':
+    config_secrets = get_secrets()
 
     client_id = config_secrets['clientId']
     secret = config_secrets['secret']
